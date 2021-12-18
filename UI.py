b7791a016668a48a49685abed4923b706b30dda2
zjhzjhhh <854314955@qq.com>
2021-12-18 20:02:24 +0800

    <Fourth> submission

--- UI.py
+++ UI.py
@@ -1,13 +1,16 @@
 import base64
 import configparser
+import csv
 import json
 import os
 import queue
 import tkinter as tk
 import webbrowser
-from tkinter import ttk
+from tkinter import ttk, messagebox, filedialog
 
+import pyperclip
 import requests
+from github import Github
 from ttkbootstrap import Style
 
 from Plugins import ICON, IP138
@@ -22,6 +25,10 @@ def login_ui():
     global item_text
     global stop_run
     global h1
+    global h2
+    global h3
+    global h4
+    global Button_1
 
     # cf = configparser.ConfigParser()
     # cf.read("./iniFile/config.ini")
@@ -42,14 +49,29 @@ def login_ui():
     menbar.add_command(label="ICON", command=ICON.create)#, command=ICON.create()
     menbar.add_command(label="IP反查", command=IP138.IP138_search)#, command=IP138.IP138_search()
     menbar.add_command(label="弱口令检测")#, command=
+    menbar.add_command(label="Google")
+
 
     window.config(menu = menbar)
 
-    h1 = tk.IntVar()
-    Checkbutton_1 = ttk.Checkbutton(window, text="排除干扰", variable=h1, offvalue=0,command=interfere)
-    Checkbutton_1.place(relx=0.040, rely=0.089, relwidth=0.090, relheight=0.040)
+    p1 = tk.IntVar()
+    Checkbutton_1 = ttk.Checkbutton(window, text="排除干扰", variable=p1, offvalue=0, command=interfere)
+    Checkbutton_1.place(relx=0.080, rely=0.089, relwidth=0.090, relheight=0.040)
 
-    Label_1 = ttk.Label(window, text="FOFA语法", font=("黑体", 11))
+    h1 = tk.BooleanVar()
+    Checkbutton_1 = ttk.Checkbutton(window, text="fofa", variable=h1, offvalue=0)#
+    Checkbutton_1.place(relx=0.280, rely=0.089, relwidth=0.090, relheight=0.040)
+
+    h2 = tk.BooleanVar()
+    Checkbutton_2 = ttk.Checkbutton(window, text="zoomeye", variable=h2, offvalue=0)#, command=zoomeye
+    Checkbutton_2.place(relx=0.480, rely=0.089, relwidth=0.090, relheight=0.040)
+
+    h3 = tk.BooleanVar()
+    Checkbutton_3 = ttk.Checkbutton(window, text="quake", variable=h3, offvalue=0)#, command=quake
+    Checkbutton_3.place(relx=0.580, rely=0.089, relwidth=0.090, relheight=0.040)
+
+
+    Label_1 = ttk.Label(window, text="语法", font=("黑体", 11))
     Label_1.place(relx=0.020, rely=0.030, relwidth=0.100, relheight=0.050)
 
     Label_2 = ttk.Label(window, text='', font=("宋体", 10))
@@ -58,13 +80,13 @@ def login_ui():
     Entry_1 = ttk.Entry(window)
     Entry_1.place(relx=0.110, rely=0.028, relwidth=0.480, relheight=0.050)
 
-    Button_1 = ttk.Button(window,text="Query", command=fofa)#, command=fofa
+    Button_1 = ttk.Button(window,text="Query", command=choose)#, command=fofa
     Button_1.place(relx = 0.845,rely =0.02,relwidth = 0.060,relheight = 0.11)
 
     Button_2 = ttk.Button(window, text="Stop")#, command=stop
     Button_2.place(relx = 0.920,rely=0.02,relwidth = 0.060,relheight = 0.05)
 
-    Button_3 = ttk.Button(window, text="Export")#, command=save
+    Button_3 = ttk.Button(window, text="Export", command=save)#, command=save
     Button_3.place(relx = 0.920,rely= 0.08,relwidth = 0.060,relheight = 0.05)
 
     col = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -106,9 +128,12 @@ def login_ui():
     tree.bind('<3>', treeviewClick)
     tree.tag_configure('oddrow',background='white')
     menu = tk.Menu(window, tearoff=False)
-    menu.add_command(label="复制URL")#, command=copyURL
-    menu.add_command(label="复制IP")#, command=copyIP
-    menu.add_command(label="复制备份")#, command=copybackup
+    menu.add_command(label="复制URL", command=copyURL)#, command=copyURL
+    menu.add_command(label="复制IP", command=copyIP)#, command=copyIP
+    #menu.add_command(label="复制备份")#, command=copybackup
+    menu.add_command(label="Github搜集",command=Github_Search)
+    #menu.add_command(label="Google搜集")
+
     tree.bind('<Double-Button-1>',gourl)
     tree.place(relx = 0.020,rely =0.140,relwidth = 0.950,relheight =0.800)
     VScroll1 = tk.Scrollbar(window, orient='vertical', command=tree.yview)
@@ -119,10 +144,39 @@ def login_ui():
 
     window.mainloop()
 
+def choose():
+    if h1.get():
+        fofa()
+    elif h2.get():
+        zoomeye()
+    elif h3.get():
+        quake()
+    elif h1.get() and h2.get():
+        fofa()
+        zoomeye()
+    elif h1.get() and h3.get():
+        fofa()
+        quake()
+    elif h2.get() and h3.get():
+        zoomeye()
+        quake()
+    elif h1.get() and h2.get() and h3.get():
+        fofa()
+        zoomeye()
+        quake()
+    else:
+        display_messagebox2()
+
+
+def zoomeye():
+    pass
+
+
+def quake():
+    pass
+
 
 def fofa():
-    global num
-    global stop_run
 
     proxies = {'http': None, 'https': None}
     cf = configparser.ConfigParser()
@@ -131,19 +185,22 @@ def fofa():
     fofa_key = cf.get('fofa api', 'KEY')
 
 
-    t = []
-    stop_run = False
-    num = 1
-    count=10000
-
-    x = tree.get_children()
+    Label_2.config(text="正在获取数据...")
+    global num
+    global stop_run
+    t=[]
+    stop_run=False
+    num=1
+    x=tree.get_children()
     for item in x:
         tree.delete(item)
-    query = Entry_1.get()
-    fields = "host,title,ip,port,domain,server"
-    query = base64.b64encode(query.encode('utf-8')).decode('utf-8')
-    r=requests.get(url="https://fofa.so/api/v1/search/all?email={}&key={}&fields={}&qbase64={}&size={}".format(fofa_email,fofa_key,fields,query,count), proxies=proxies)
-    data = json.loads(r.text.encode("GBK", 'ignore').decode('GBK'))
+    query=Entry_1.get()
+    count=10000
+    fields="host,title,ip,port,domain,server"
+    query=base64.b64encode(query.encode('utf-8')).decode('utf-8')
+    r=requests.get(url="https://fofa.so/api/v1/search/all?email={}&key={}&fields={}&qbase64={}&size={}".format(fofa_email,fofa_key,fields,query,count),proxies=proxies)
+    data=json.loads(r.text.encode("GBK",'ignore').decode('GBK'))
+
     for i in data['results']:
         if 'http' in i[0]:
             host=i[0]
@@ -152,13 +209,15 @@ def fofa():
                 host='https://'+i[0]
             else:
                 host='http://'+i[0]
-        title = i[1].strip()
-        ip = i[2]
-        prot = i[3]
-        domain = i[4]
-        server = i[5]
-        result(host, title, ip, prot, domain, server, '')
+        title=i[1].strip()
+        ip=i[2]
+        prot=i[3]
+        domain=i[4]
+        server=i[5]
+        result(host,title,ip,prot,domain,server,'')
     Label_2.config(text="扫描任务已结束")
+# time.sleep(60)
+
 
 
 def result(host,title,ip,port,domain,server,backup=None):
@@ -194,6 +253,140 @@ def interfere():
     elif h1.get() ==1:
         h1.set(0)
 
+def save():
+    li=[]
+    file_path=filedialog.asksaveasfilename(initialdir=os.path.abspath('.'),title=u'保存文件',filetypes=[('csv File','.csv')])
+    f=open(file_path+'.csv','a+',encoding='utf_8_sig',newline='')
+    winter=csv.writer(f)
+    column=['序号','HOST','','','','','','','']
+    winter.writerow(column)
+    for row_id in tree.get_children():
+        row=tree.item(row_id)
+        li.append(row['values'])
+    winter.writerows(li)
+
+
+def display_messagebox2():
+	messagebox.showinfo(title='提示',
+		message='需要选择功能')
+
+def copyIP():
+    global item_text
+    pyperclip.copy(item_text[2])
+
+def copyURL():
+    global item_text
+    pyperclip.copy(item_text[1])
+
+def Github_Search():
+    global num
+    num = 1
+    global e1
+    global top
+    top = tk.Toplevel()
+    top.title('github搜索')
+    top.wm_attributes("-topmost",1)
+    top.geometry("400x100+550+250")
+    top.resizable(width=False, height=False)
+    L1=ttk.Label(top,text="关键字:",font=("宋体",9))
+    L1.place(relx = 0.020,rely =0.180,relwidth =0.30,relheight = 0.30)
+    e1=ttk.Entry(top)
+    e1.place(relx = 0.2,rely =0.25,relwidth = 0.600,relheight = 0.2)
+    c=ttk.Button(top,text="搜索",command=Github_Search_Gui)
+    c.place(relx = 0.820,rely =0.22,relwidth = 0.140,relheight = 0.25)
+
+def Github_Search_Gui():
+    cf = configparser.ConfigParser()
+    cf.read("./config/config.ini")
+    github_token = cf.get('github api', 'GITHUB_TOKEN')
+
+
+
+    x=tree.get_children()
+    for item in x:
+        tree.delete(item)
+    tree.heading('1',text="序号")
+    tree.heading('2',text='URL')
+    tree.heading('3',text='关键字')
+    tree.heading('4',text="误报")
+    tree.column('1',width=150,anchor='center')
+    tree.column('2',width=720,anchor='center')
+    tree.column('3',width=200,anchor='center')
+    tree.column('4',width=200,anchor='center')
+    query=e1.get()
+    hosts=query.split(',')
+    for i in range(0,len(hosts)):
+        hosts.append(hosts[i]+" @")
+    print(hosts)
+    keywords=GenerateKeywords(hosts)
+    github_test=Github(github_token)
+    for key in keywords:
+        result=github_test.search_code(
+            key,
+            sort="indexed",
+            order="desc",
+        )
+        analysis_page(result,key)
+def analysis_page(result,key):
+    page_id=0
+    while page_id < 34:
+        try:
+            iteks=result.get_page(page_id)
+            ana_result=analysis_result(iteks,key)
+            if not ana_result:
+                print("[WARNING] 在第{}页退出".format(page_id))
+                break
+        except Exception as e:
+            print(e)
+    page_id+=1
+    print("[INFO] 结束关键字: " + key + "\n\n")
+
+
+def analysis_result(items, key):
+    result_id = 0
+    result_count = len(items)
+
+    if not result_count:
+        result("目标为空", key, "", "", "", "")
+        return None
+
+    while result_id < result_count:
+        item = items[result_id]
+        try:
+            if all(list([kw in item.decoded_content.decode("utf8") for kw in key.split(" ")])):
+                negative = "疑似"
+            else:
+                negative = "False"
+            url = "https://www.github.com/" + \
+                  item.repository.full_name + "/blob/master/" + item.path
+            print(url)
+            update_result(url, key, negative, "", "", "")
+
+        except Exception as e:
+            print(e)
+        result_id += 1
+def update_result(host,title,ip,port,domain,server,backup=None,Fingerprint=None):
+    global num
+    li=[num,host,title,ip,port,domain,server,backup,Fingerprint]
+    num=int(num)+1
+    if (num % 2)==0:
+        tree.insert('', 'end', values=li,tags=('oddrow',))
+        tree.update()
+    else:
+        tree.insert('', 'end', values=li)
+        tree.update()
+def GenerateKeywords(hosts):
+    key = ['jdbc:', 'password', 'username', 'database', 'smtp', 'vpn', 'pwd', 'passwd', 'connect', "密码"]
+    keywords = []
+
+    for h in hosts:
+        if "@" in h:
+            h = h.split("@")[0] + " smtp"
+
+        for k in key:
+            keywords.append(h + " " + k)
+
+    return keywords
 if __name__ == '__main__':
     login_ui()
 
